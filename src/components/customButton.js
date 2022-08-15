15a29df0af0e7c130180aad4adf588ee012cb9e4
TheCubiq <jlajsek@gmail.com>
2022-08-15 21:30:41 +0200

    ✨ add first component and load it

---
+++ src/components/customButton.js
@@ -0,0 +1,29 @@
+const myButton = () => {
+  let button = document.createElement("button");
+  button.innerHTML = "Vizzard";
+  // button.style.position = "absolute";
+  button.onclick = function () {
+    // alert("You clicked me!");
+    console.log("You clicked me!");
+  };
+
+  // learning simple event listeners
+  button.addEventListener(
+    "mouseover",
+    function () {
+      button.style.backgroundColor = "red";
+    },
+    false
+  );
+  button.addEventListener(
+    "mouseout",
+    function () {
+      button.style.backgroundColor = "";
+    },
+    false
+  );
+
+  return button;
+};
+
+export default myButton;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
+import myButton from "./components/customButton";
 import vizzyThemer from "./styles/vizzyThemer.css?inline";
 
 function run() {
   // if (self != top) return
   GM_addStyle(vizzyThemer);
+  injectVizzard();
   console.log("Vizzard Has Loaded!");
+}
+
+const injectVizzard = () => {
+  const header = document.querySelector("header");
+  const playPanel = document.getElementsByClassName("MuiDivider-flexItem")[1]
+    .parentElement;
+
+  header.appendChild(myButton());
+  playPanel.appendChild(myButton());
 }
 
 run();
