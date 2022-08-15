1bcbc3876a3ca3d3188b3370b7235bb1754fb4bf
TheCubiq <jlajsek@gmail.com>
2022-08-15 21:28:44 +0200

    🖋 add Eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "greasemonkey",
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
