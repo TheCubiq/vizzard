5b28e339fc76f01571c86facf6013da64373ce19
TheCubiq <jlajsek@gmail.com>
2022-08-18 03:24:39 +0200

    🌍Nice

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import Userscript from 'vite-plugin-tm-userscript'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 6969,
+  },
   plugins: [
     Userscript({
       entry: 'src/main.js',
