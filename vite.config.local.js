6d3298665164883859a5d6be864eb980dce2b272
TheCubiq <jlajsek@gmail.com>
2022-08-22 14:33:31 +0200

    🖋 vite config local

--- vite.config.local.js
+++ vite.config.local.js
@@ -4,20 +4,20 @@ import Userscript from 'vite-plugin-tm-userscript'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 6969,
+    port: 7979,
   },
   plugins: [
     Userscript({
       entry: 'src/main.js',
       headers: {
-        'name': 'Vizzard',
+        'name': 'Vizzard-Local',
         'author': 'Cubiq The Creator',
         'supportURL': 'https://github.com/TheCubiq/vizzard',
         'namespace': 'https://github.com/TheCubiq/vizzard',
         'license': 'GPL-3.0',
         'match': 'https://vizzy.io/*',
         'icon': 'https://vizzy.io/favicon.ico',
-        'require': 'https://raw.githubusercontent.com/TheCubiq/vizzard/master/versionCheck.js',
+        'require': 'file:///D:/Users/kubaj/programovani/js/vizzard/versionCheck.js',
         'description': 'customize the look and feel of the vizzy website!',
       },
     }),
