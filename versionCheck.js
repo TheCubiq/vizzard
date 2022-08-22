6c8353b4077d29c197de8f492335c3e09658829c
TheCubiq <jlajsek@gmail.com>
2022-08-22 18:37:05 +0200

    🤯UserScript VersionCheck

--- versionCheck.js
+++ versionCheck.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name              VizzardVers
+// @description       check the vers
+// @author            Cubiq The Creator
+// @namespace         https://github.com/TheCubiq/vizzardVers
+// @match             https://vizzy.io/*
+// @icon              https://vizzy.io/favicon.ico
+// @supportURL        https://github.com/TheCubiq/vizzard
+// @grant             unsafeWindow
+// ==/UserScript==
 "use strict";
 const VIZZARD_LATEST_VERSION = '0.0.70-1';
 unsafeWindow.VIZZARD_LATEST_VERSION = VIZZARD_LATEST_VERSION;
