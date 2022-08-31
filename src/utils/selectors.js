47faae33e069100778e0b29268accd9c501934e9
TheCubiq <jlajsek@gmail.com>
2022-08-31 20:51:06 +0200

    ✨ new selectors

--- src/utils/selectors.js
+++ src/utils/selectors.js
@@ -1,13 +1,15 @@
 export default function Selectors() {
+  const panels = document.querySelectorAll('.mosaic-tile');
   return {
     editor: document.querySelector("#editor-base"),
+    rootStyle: document.documentElement.style,
     header: document.querySelector("header"),
     headerButtons: document.querySelector("#editor-base > header > div:nth-child(2)"),
+    panels,
+    fullBottomPanel: panels[3],
     popupDialog: document.querySelector("body > div.MuiDialog-root > div.MuiDialog-container.MuiDialog-scrollPaper > div"),
     playPanel: document.getElementsByClassName("MuiDivider-flexItem")[1]?.parentElement,
-    rootStyle: document.documentElement.style,
     clipPanel: document.getElementsByClassName("ps")[3],
-    fullBottomPanel: document.querySelector("#editor-base > div > div > div.mosaic-root > div:nth-child(7)"),
     vizzyLogo: document.querySelector("#root > div > header > div.MuiToolbar-root.MuiToolbar-regular.MuiToolbar-gutters > div > div:nth-child(1) > div"),
     vizzyLogoEditor: document.querySelector("#editor-base > header > .MuiBox-root"),
   };
